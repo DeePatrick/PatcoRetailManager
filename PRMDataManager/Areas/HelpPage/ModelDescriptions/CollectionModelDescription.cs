401cd2f7f0589fea88f7d070f67377032dba622b
chukwudozie <patrick.okudo@quicklinkconsultancy.com>
2020-08-13 14:42:13 +0100

    Added an MVC application to Solution
    Added C# Data layer project with Api and MVC and individual authenthication

---
+++ PRMDataManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PRMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ PRMDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PRMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
