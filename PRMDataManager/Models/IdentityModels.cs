1b9fa3fb4318308d357e0e7d58a3f52c4bd267dc
Chukwudozie Okudo <okudopato@yahoo.com>
2020-09-04 15:03:57 +0100

    Changed EF DB
    Removed original aspnet db and renamed it From DefaultConnection to EFData

--- PRMDataManager/Models/IdentityModels.cs
+++ PRMDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace PRMDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
