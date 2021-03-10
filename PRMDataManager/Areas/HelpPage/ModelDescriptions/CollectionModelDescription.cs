8ee71971530a586851f9fdb4d1f74f21fad5508d
chukwudozie <patrick.okudo@quicklinkconsultancy.com>
2021-03-10 11:21:45 +0000

    remove old API

--- PRMDataManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace PRMDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- PRMDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PRMDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
